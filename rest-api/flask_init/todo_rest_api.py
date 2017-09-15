8618f0d1e2c93c2a635ace9462364b115eed8574
felipe <felipe.basina@gmail.com>
2017-09-14 21:58:13 -0300

    Adicionado operacoes http para teste CRUD.

--- rest-api/flask_init/todo_rest_api.py
+++ rest-api/flask_init/todo_rest_api.py
@@ -3,17 +3,34 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
-
+# Cria um dicionario para armazenagem de dados em memoria
 todos = {}
 
 class TodoSimple(Resource):
     def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+        try:
+            return {todo_id: todos[todo_id]}
+        except:
+            return {"erro": "Nenhum registro encontrado com id " + todo_id, 
+                    "status": 500}
 
     def put(self, todo_id):
         todos[todo_id] = request.form['data']
         return {todo_id: todos[todo_id]}
+        
+    def post(self, todo_id):
+        content = request.get_json(silent=True)
+        todos[todo_id] = content
+        return {todo_id: content}
 
+    def delete(self, todo_id):
+        try:
+            del todos[todo_id]
+            return todos
+        except:
+            return {"erro": "Nenhum registro encontrado para remocao com id " + todo_id, 
+                    "status": 400}
+        
 api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
