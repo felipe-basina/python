34ba6243e62938d2ed693e5c2447c11d7021fd0e
felipe <felipe.basina@gmail.com>
2017-09-19 21:29:38 -0300

    Adicionado arquivos para teste com sqlite3.

---
+++ sqlite3/06_read_data.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('clientes.db')
+cursor = conn.cursor()
+
+# lendo os dados
+cursor.execute("""
+SELECT * FROM clientes;
+""")
+
+for linha in cursor.fetchall():
+    print(linha)
+
+conn.close()
---
+++ sqlite3/12_read_sql.py
@@ -0,0 +1,14 @@
+import sqlite3
+import io
+
+conn = sqlite3.connect('clientes_recuperado.db')
+cursor = conn.cursor()
+
+f = io.open('clientes_dump.sql', 'r')
+sql = f.read()
+cursor.executescript(sql)
+
+print('Banco de dados recuperado com sucesso.')
+print('Salvo como clientes_recuperado.db')
+
+conn.close()
