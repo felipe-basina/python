2eff2dfc14ebf20185a343e4f30fa06d85a2e41e
felipe <felipe.basina@gmail.com>
2017-10-04 22:01:54 -0300

    Adicionado projeto mongodb.

---
+++ nosql/mongodb/oficial_01/createsamples.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+from random import randint
+
+#Step 1: Connect to MongoDB - Note: Change connection string as needed
+client = MongoClient(port=27017)
+db = client.business
+
+#Step 2: Create sample data
+names = ['Kitchen','Animal','State', 'Tastey', 'Big','City','Fish', 'Pizza','Goat', 'Salty','Sandwich','Lazy', 'Fun']
+company_type = ['LLC','Inc','Company','Corporation']
+company_cuisine = ['Pizza', 'Bar Food', 'Fast Food', 'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
+
+max_range = 501
+for x in range(1, max_range):
+    business = {
+        'name' : names[randint(0, (len(names)-1))] + ' ' + names[randint(0, (len(names)-1))]  + ' ' + company_type[randint(0, (len(company_type)-1))],
+        'rating' : randint(1, 5),
+        'cuisine' : company_cuisine[randint(0, (len(company_cuisine)-1))] 
+    }
+    #Step 3: Insert business object directly into MongoDB via insert_one
+    result = db.reviews.insert_one(business)
+    #Step 4: Print to the console the ObjectID of the new document
+    print('Created {0} of {1} as {2}'.format(x, max_range, result.inserted_id))
+#Step 5: Tell us that you are done
+print('finished creating %s business reviews' % max_range)
