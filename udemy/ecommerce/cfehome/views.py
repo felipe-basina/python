27c6229108f34ccd560572955e5c789f1c8344d2
felipe <felipe.basina@gmail.com>
2017-11-25 10:16:42 -0200

    Ajustes no codigo; implementado cadastro de usuario.

--- udemy/ecommerce/cfehome/views.py
+++ udemy/ecommerce/cfehome/views.py
@@ -1,15 +1,17 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .forms import ContactForm, LoginForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
     print('home_page')
     context = {
         "title": "Hello world!",
-        "content": "Welcome to the home page"
+        "content": "Welcome to the home page",
     }
+    if request.user.is_authenticated():
+        context["premium_content"] = "YEAHHHHH"
     return render(request, "home_page.html", context)
 
 def about_page(request):
@@ -61,17 +63,26 @@ def login_page(request):
             #print(request.user.is_authenticated())
             login(request, user)
             #context['form'] = LoginForm() # Reinicia a instancia
-            return redirect("/login/")
+            return redirect("/")
         else:
             print("Error")
         
     return render(request, "auth/login.html", context)
 
+User = get_user_model()
 def register_page(request):
-    form = LoginForm(request.POST or None)
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
     if form.is_valid():
         print(form.cleaned_data)
-    return render(request, "auth/register.html", {})
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, "auth/register.html", context)
     
 def home_page_old(request):
     html_ = """
