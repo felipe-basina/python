9cad8e02e5b3bb0a8bcf52741730b2b046a27356
felipe <felipe.basina@gmail.com>
2017-11-29 23:01:35 -0200

    Ajustes para adicionar custom managers a ser utilizado para recuperar registros da base de dados.

--- udemy/ecommerce/products/models.py
+++ udemy/ecommerce/products/models.py
@@ -17,11 +17,21 @@ def upload_image_path(instance, filename):
             new_filename=new_filename, 
             final_filename=final_filename)
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        # Equiavalente chamar Product.objects...
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+            
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    
+    objects = ProductManager()
     
     def __str__(self):
         return self.title
--- udemy/ecommerce/products/views.py
+++ udemy/ecommerce/products/views.py
@@ -22,13 +22,38 @@ def product_list_view(request):
     return render(request, "products/list.html", context)
     
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get("pk")
+        instance = Product.objects.get_by_id(id=pk)
+        print("instance - <from get_object>", instance)
+        if instance is None:
+            raise Http404("Product %d doesn't exist" % int(pk))
+        return instance
+    
+    '''
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get("pk")
+        print("id from get_queryset", pk)
+        return Product.objects.filter(pk=pk)
+    '''
+    
 def product_detail_view(request, pk):
     #instance = Product.objects.get(pk=pk)
-    instance = get_object_or_404(Product, pk=pk)
+    #instance = get_object_or_404(Product, pk=pk)
+    instance = Product.objects.get_by_id(id=pk)
     print("instance:", instance)
+    if instance is None:
+        raise Http404("Product %d doesn't exist" % int(pk))
     '''
     try:
         instance = Product.objects.get(id=pk)
@@ -48,4 +73,5 @@ def product_detail_view(request, pk):
     context = {
         "object": instance
     }
+    print("context", context)
     return render(request, "products/detail.html", context)
