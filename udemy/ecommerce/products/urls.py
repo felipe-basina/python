d9c49a450b101397cb09eeef594f9cb76378a0c6
felipe <felipe.basina@gmail.com>
2017-12-14 22:06:03 -0200

    Implementacao de urls reversas.

--- udemy/ecommerce/products/urls.py
+++ udemy/ecommerce/products/urls.py
@@ -7,5 +7,5 @@ from .views import (
 
 urlpatterns = [
     url(r'^$', ProductListView.as_view()),
-    url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
+    url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='detail'),
 ]
