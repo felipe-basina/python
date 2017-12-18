2d126e1d7ed3da3b3e7e2d63fb3255f7ede5df57
felipe <felipe.basina@gmail.com>
2017-12-18 21:30:59 -0200

    Ajustes na definicao de urls reversas; adicionado implementacao para navbar.

--- udemy/ecommerce/products/models.py
+++ udemy/ecommerce/products/models.py
@@ -58,7 +58,7 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         #return "/products/{slug}/".format(slug=self.slug)
-        return reverse("detail", kwargs={"slug": self.slug})
+        return reverse("products:detail", kwargs={"slug": self.slug})
     
     def __str__(self):
         return self.title
