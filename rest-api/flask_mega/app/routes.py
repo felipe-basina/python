c4f879221beffc97aca64b65eafae4083edbee6f
felipe.nascimento.ecommet@gmail.com <felipe.nascimento.ecommet@gmail.com>
2017-12-22 12:48:51 -0200

    Adicionado templates e refatorado view.

--- rest-api/flask_mega/app/routes.py
+++ rest-api/flask_mega/app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return "Hello World"
+    user = {'username': 'flasker'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    #return render_template('index.html', title='home', user=user)
+    return render_template('index.html', user=user, posts=posts)
