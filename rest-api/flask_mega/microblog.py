2623923697310084af76cb324e76913892104670
felipe.nascimento.ecommet@gmail.com <felipe.nascimento.ecommet@gmail.com>
2017-12-29 10:05:17 -0200

    Adicionado implementacao para integrar com base de dados sqlite.

--- rest-api/flask_mega/microblog.py
+++ rest-api/flask_mega/microblog.py
@@ -1,1 +1,12 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+'''
+ Utilizado para que se possa iniciar
+ o flask shell e nao ter a necessidade
+ de sempre importar os pacotes para realizacao
+ de testes. Comando: flask shell
+'''
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}
