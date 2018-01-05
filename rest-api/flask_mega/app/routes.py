28133b294ece60c81793071cab5634f23eed4a0e
felipe.nascimento.ecommet@gmail.com <felipe.nascimento.ecommet@gmail.com>
2018-01-05 16:38:32 -0200

    Inicio desenvolvimento da funcionalidade de login.

--- rest-api/flask_mega/app/routes.py
+++ rest-api/flask_mega/app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for, jsonify
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
 
 @app.route("/")
 @app.route("/index")
