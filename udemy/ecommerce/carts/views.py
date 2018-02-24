bd9d4e46b8e9e9331f1389caaeec6c565cc4593a
felipe <felipe.basina@gmail.com>
2018-02-24 11:36:55 -0300

    Implementado criacao/recuperacao de cart na sessao.

--- udemy/ecommerce/carts/views.py
+++ udemy/ecommerce/carts/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
+from .models import Cart
+
+def cart_create(user=None):
+	cart_obj = Cart.objects.create(user=None)
+	print("New cart created")
+	return cart_obj
+
 def cart_home(request):
 	cart_id = request.session.get("cart_id", None)
-	if cart_id is None: # and isinstance(cart_id, int):
-		print("Create new cart")
-		request.session['cart_id'] = 12
-		pass
+	'''
+	if cart_id is None:
+		cart_obj = cart_create()
+		request.session['cart_id'] = cart_obj.id
 	else:
+	'''
+	qs = Cart.objects.filter(id=cart_id)
+	if qs.count() == 1:
 		print("Cart ID exists")
+		cart_obj = qs.first()
+	else:
+		cart_obj = cart_create()
+		request.session['cart_id'] = cart_obj.id
 	return render(request, "carts/home.html", {})
