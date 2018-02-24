af11e8c89a054b9e492b8785ec138cc0e804cb5e
felipe <felipe.basina@gmail.com>
2018-02-24 15:53:03 -0300

    Implementacao do botao 'Adicionar ao cart.

--- udemy/ecommerce/carts/views.py
+++ udemy/ecommerce/carts/views.py
@@ -18,13 +18,18 @@ def cart_home(request):
 	return render(request, "carts/home.html", {})
 
 def cart_update(request):
-	print(request.POST)
-	product_obj = Product.objects.get(id=1)
-	cart_obj, new_obj = Cart.objects.new_or_get(request)
+	product_id = request.POST.get('product_id')
+	if product_id is not None:
+		try:
+			product_obj = Product.objects.get(id=product_id)
+		except Product.DoesNotExist:
+			print("Show message to user, product is gone?")
+			return redirect("carts:home")		
+		cart_obj, new_obj = Cart.objects.new_or_get(request)
 
-	if product_obj in cart_obj.products.all():
-		cart_obj.products.remove(product_obj)
-	else:
-		cart_obj.products.add(product_obj)
+		if product_obj in cart_obj.products.all():
+			cart_obj.products.remove(product_obj)
+		else:
+			cart_obj.products.add(product_obj)
 	#return redirect(product_obj.get_absolute_url())
 	return redirect("carts:home")
