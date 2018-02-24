1131a35211fb51a4619d68921305f84ba5d6ea03
felipe <felipe.basina@gmail.com>
2018-02-24 11:20:52 -0300

    Adicionado modelo e migracao para cart.

--- udemy/ecommerce/carts/views.py
+++ udemy/ecommerce/carts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 def cart_home(request):
-	#print(request.session)
-	#print(dir(request.session)) # Metodo utilizado para visualizar os metodos disponiveis no objeto
-	# request.session.set_expiry(300) # 5 minutos
-	#key = request.session.session_key
-	#print(key)
-	#request.session['first_name'] = "Felipe"
+	cart_id = request.session.get("cart_id", None)
+	if cart_id is None: # and isinstance(cart_id, int):
+		print("Create new cart")
+		request.session['cart_id'] = 12
+		pass
+	else:
+		print("Cart ID exists")
 	return render(request, "carts/home.html", {})
