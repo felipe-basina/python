b39eb624b3b9979ec579c569dd9a18b5f4e79c26
felipe.nascimento.ecommet@gmail.com <felipe.nascimento.ecommet@gmail.com>
2018-02-27 08:16:58 -0300

    Ajuste no codigo para melhor visualizar o processamento assincrono.

--- codigos/async_call.py
+++ codigos/async_call.py
@@ -5,13 +5,14 @@ pool = ThreadPool(processes=1)
 
 def teste_thread():
   for k in range(5):
-    print('Executando Thread')
+    print('Executando Thread...\n')
     time.sleep(1)
   return 'Thread Executada com sucesso!'
 
 def exec():
     async_call = pool.apply_async(teste_thread)
     print('Processando....')
+    time.sleep(10)
     return async_call.get()
   
 if __name__ == '__main__':
