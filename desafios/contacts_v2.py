673bd551dbc6d295b384bf96bcfcacb5b172fbb7
fnascimento <felipe.basina@gmail.com>
2020-04-09 23:39:31 -0300

    Removido funcao nao utilizada.

--- desafios/contacts_v2.py
+++ desafios/contacts_v2.py
@@ -1,11 +1,5 @@
 from functools import reduce
 
-def count_terms(names, term):
-    value = reduce(lambda x, y: x + y, 
-            map(lambda contact: 1 if contact != None and term in contact else 0, names), 0)
-    print(value)
-    return value
-
 def contacts(queries):
     names = [name for name in list(map(lambda el: el[1] if 'add' == el[0] else None, queries)) if name != None]
     terms = [term for term in list(map(lambda el: el[1] if 'find' == el[0] else None, queries)) if term != None]
