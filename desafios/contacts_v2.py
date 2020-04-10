b934fe3416edc5f5aee5576a623f02edd7690ad6
fnascimento <felipe.basina@gmail.com>
2020-04-09 23:36:33 -0300

    Adicionado versao 2 do desafio de contatos.

---
+++ desafios/contacts_v2.py
@@ -0,0 +1,34 @@
+from functools import reduce, partial
+
+def count_terms(names, term):
+    value = reduce(lambda x, y: x + y, 
+            map(lambda contact: 1 if contact != None and term in contact else 0, names), 0)
+    print(value)
+    return value
+
+def contacts(queries):
+    names = [name for name in list(map(lambda el: el[1] if 'add' == el[0] else None, queries)) if name != None]
+    terms = [term for term in list(map(lambda el: el[1] if 'find' == el[0] else None, queries)) if term != None]
+
+    count_arr = []
+    for term in terms:
+        if term != None:
+            count = reduce(lambda x, y: x + y, 
+                            map(lambda contact: 1 if contact != None and term in contact else 0, names), 0)
+            count_arr.append(count)
+
+    return count_arr
+
+queries = [['add', 's'],
+            ['add', 'ss'],
+            ['add', 'sss'],
+            ['add', 'ssss'],
+            ['add', 'sssss'],
+            ['find', 's'],
+            ['find', 'ss'],
+            ['find', 'sss'],
+            ['find', 'ssss'],
+            ['find', 'sssss'],
+            ['find', 'ssssss']]
+for total in contacts(queries):
+    print(total)
